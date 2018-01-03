8f3e85ae993ba537d54d0bdedbb418dcbbea0e9d
im宇 <yitozji@163.com>
2018-01-03 15:08:37 +0800

    完成html静态文件跨module访问

--- home-web/src/main/java/me/imyu/home/web/ControllerExceptionHandler.java
+++ home-web/src/main/java/me/imyu/home/web/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package me.imyu.home.web;
 
+import me.imyu.home.base.model.dto.ResultBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
