ddc843eacd4e6e1a30106f474f89e9eac1113d0c
gittozji <yitozji@163.com>
2018-03-04 20:56:40 +0800

    1、修复cookie空指针异常，遗留jwt解析异常。2、引入前端框架

--- home-web/src/main/java/me/imyu/home/web/interceptor/JwtBinderInterceptor.java
+++ home-web/src/main/java/me/imyu/home/web/interceptor/JwtBinderInterceptor.java
@@ -3,6 +3,8 @@ package me.imyu.home.web.interceptor;
 import me.imyu.home.auth.jwt.Jwt;
 import me.imyu.home.auth.jwt.JwtBinder;
 import me.imyu.home.auth.jwt.JwtParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -14,20 +16,26 @@ import javax.servlet.http.HttpServletResponse;
  * Created by imyu on 2018-02-13.
  */
 public class JwtBinderInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtBinderInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = "";
 
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if ("token".equals(cookie.getName())) {
-                token = cookie.getValue();
-                break;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("token".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                    break;
+                }
             }
+        } else {
+            LOGGER.info("没有cookies");
         }
 
         if (token.isEmpty()) {
             JwtBinder.bind(null);
+            LOGGER.info("没有token");
         } else {
             Jwt jwt = JwtParser.parseJWT(token);
             JwtBinder.bind(jwt);
